6c0d6317a4ca6b03e6bb7c35d30c5a13d22f97fd
Maik <maicolestebancastillolopez@gmail.com>
2024-04-22 10:00:20 -0500

    actividad 2 correccion
    cambios

--- src/main/java/com/mycompany/libropoo/Libro.java
+++ src/main/java/com/mycompany/libropoo/Libro.java
@@ -1,41 +1,37 @@
 package com.mycompany.libropoo;
 
 public class Libro {
-    private String autor;
     private String titulo;
+    private String autor;
     private int anoPublicacion;
     private String genero;
     private boolean leido;
-    
 
-    Libro(String autor, String titulo, int anoPublicacion, String genero){
-    this.autor = autor;
-    this.titulo = titulo;
-    this.anoPublicacion = anoPublicacion;
-    this.genero = genero;
+    Libro(String autor, String titulo, int anoPublicacion, String genero) {
+        this.autor = autor;
+        this.titulo = titulo;
+        this.anoPublicacion = anoPublicacion;
+        this.genero = genero;
+    }
+//complejidad temporal : O(1)
+    public String mostrarInformacion() {
+        String info = "Titulo:[" + titulo + "], Autor:[" + autor + "], Año de publicaciòn:[" + anoPublicacion
+                + "], Genero:[" + genero + "]";
+
+        return info;
+    }
+//complejidad temporal : O(1)
+    public void marcarLeido() {
+        leido = true;
+
+    }
+//complejidad temporal : O(1)
+    public boolean esAntiguo() {
+        return (2024 - anoPublicacion >= 50);
+    }
+//complejidad temporal : O(1)
+    public boolean getLeido() {
+        return leido;
     }
 
-    public String mostrarInformacion(){
-    String info = "Titulo:["+titulo+"], Autor:["+autor+"], Año de publicaciòn:["+anoPublicacion+"], Genero:["+genero+"]";
-
-
-     return info;
-    }
-    public void marcarLeido(){
-    leido = true;
-
-
-    }
-
-    
-    public boolean esAntiguo(){
-     return(2024 - anoPublicacion>=50);
-        }
-   
-
-      }     
-
-       
-    
-
-
+}
--- src/main/java/com/mycompany/libropoo/Principal.java
+++ src/main/java/com/mycompany/libropoo/Principal.java
@@ -1,26 +1,26 @@
 package com.mycompany.libropoo;
+
 import java.util.Scanner;
+
 public class Principal {
     public static void main(String[] args) {
-        
-    
-Scanner sc = new Scanner(System.in);
 
-    String tituloSc = sc.nextLine();
-    String autorSc = sc.nextLine();
-    int anoPublicacionSc = sc.nextInt();
-    String generoSc = sc.next();
+        Scanner sc = new Scanner(System.in);
 
-    Libro lectura = new Libro(tituloSc, autorSc, anoPublicacionSc, generoSc);
+        String tituloSc = sc.nextLine();
+        String autorSc = sc.nextLine();
+        int anoPublicacionSc = sc.nextInt();
+        sc.nextLine();
+        String generoSc = sc.nextLine();
 
-   
-   
- 
+        Libro lectura = new Libro(tituloSc, autorSc, anoPublicacionSc, generoSc);
+
         System.out.println(lectura.mostrarInformacion());
+        System.out.println(lectura.getLeido());
         lectura.marcarLeido();
-        System.out.println("el libro ha sido leido");    
+        System.out.println(lectura.getLeido());
+        System.out.println("el libro ha sido leido");
         System.out.println(lectura.esAntiguo());
-
 
     }
 }

913bab307509efadddf5fc425f9cc6c93c1be3da
Maik <maicolestebancastillolopez@gmail.com>
2024-04-22 09:51:13 -0500

    actividad 2
    actividad 2 programaciòn orientada a objetos

--- src/main/java/com/mycompany/libropoo/Libro.java
+++ src/main/java/com/mycompany/libropoo/Libro.java
@@ -1,5 +1,41 @@
 package com.mycompany.libropoo;
 
 public class Libro {
+    private String autor;
+    private String titulo;
+    private int anoPublicacion;
+    private String genero;
+    private boolean leido;
     
-}
+
+    Libro(String autor, String titulo, int anoPublicacion, String genero){
+    this.autor = autor;
+    this.titulo = titulo;
+    this.anoPublicacion = anoPublicacion;
+    this.genero = genero;
+    }
+
+    public String mostrarInformacion(){
+    String info = "Titulo:["+titulo+"], Autor:["+autor+"], Año de publicaciòn:["+anoPublicacion+"], Genero:["+genero+"]";
+
+
+     return info;
+    }
+    public void marcarLeido(){
+    leido = true;
+
+
+    }
+
+    
+    public boolean esAntiguo(){
+     return(2024 - anoPublicacion>=50);
+        }
+   
+
+      }     
+
+       
+    
+
+
--- src/main/java/com/mycompany/libropoo/Principal.java
+++ src/main/java/com/mycompany/libropoo/Principal.java
@@ -1,5 +1,26 @@
 package com.mycompany.libropoo;
-
+import java.util.Scanner;
 public class Principal {
+    public static void main(String[] args) {
+        
     
+Scanner sc = new Scanner(System.in);
+
+    String tituloSc = sc.nextLine();
+    String autorSc = sc.nextLine();
+    int anoPublicacionSc = sc.nextInt();
+    String generoSc = sc.next();
+
+    Libro lectura = new Libro(tituloSc, autorSc, anoPublicacionSc, generoSc);
+
+   
+   
+ 
+        System.out.println(lectura.mostrarInformacion());
+        lectura.marcarLeido();
+        System.out.println("el libro ha sido leido");    
+        System.out.println(lectura.esAntiguo());
+
+
+    }
 }
